37523d8707c733e36bb5df4c8d903add8650c035
许迎春 <yingchun.xu@china.zhaogang.com>
2020-09-22 20:21:31 +0800

    format

--- ui_components/cef_control/app/client_app.h
+++ ui_components/cef_control/app/client_app.h
@@ -14,62 +14,60 @@
 #include "include/cef_app.h"
 #include "cef_control/app/cef_js_bridge.h"
 
-namespace nim_comp
-{
-class ClientApp : public CefApp,
-                  public CefBrowserProcessHandler,
-                  public CefRenderProcessHandler
-{
-public:
-	ClientApp();
+namespace nim_comp {
+    class ClientApp : public CefApp,
+        public CefBrowserProcessHandler,
+        public CefRenderProcessHandler {
+      public:
+        ClientApp();
 
-private:
-	// CefApp methods.
-	virtual void OnBeforeCommandLineProcessing(const CefString& process_type, CefRefPtr<CefCommandLine> command_line) OVERRIDE;
-	virtual void OnRegisterCustomSchemes( CefRefPtr<CefSchemeRegistrar> registrar) OVERRIDE;
-	virtual CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler() OVERRIDE{ return this; }
-	virtual CefRefPtr<CefRenderProcessHandler> GetRenderProcessHandler() OVERRIDE{ return this; }
+      private:
+        // CefApp methods.
+        virtual void OnBeforeCommandLineProcessing(const CefString& process_type, CefRefPtr<CefCommandLine> command_line) OVERRIDE;
+        virtual void OnRegisterCustomSchemes( CefRefPtr<CefSchemeRegistrar> registrar) OVERRIDE;
+        virtual CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler() OVERRIDE{ return this; }
+        virtual CefRefPtr<CefRenderProcessHandler> GetRenderProcessHandler() OVERRIDE{ return this; }
 
-	// CefBrowserProcessHandler methods.
-	virtual void OnContextInitialized() OVERRIDE;
-	virtual void OnBeforeChildProcessLaunch(
-		CefRefPtr<CefCommandLine> command_line) OVERRIDE;
-	virtual void OnRenderProcessThreadCreated(CefRefPtr<CefListValue> extra_info) OVERRIDE;
+        // CefBrowserProcessHandler methods.
+        virtual void OnContextInitialized() OVERRIDE;
+        virtual void OnBeforeChildProcessLaunch(
+            CefRefPtr<CefCommandLine> command_line) OVERRIDE;
+        virtual void OnRenderProcessThreadCreated(CefRefPtr<CefListValue> extra_info) OVERRIDE;
 
-	// CefRenderProcessHandler methods.
-	virtual void OnRenderThreadCreated(CefRefPtr<CefListValue> extra_info) OVERRIDE;
-	virtual void OnWebKitInitialized() OVERRIDE;
-	virtual void OnBrowserCreated(CefRefPtr<CefBrowser> browser) OVERRIDE;
-	virtual void OnBrowserDestroyed(CefRefPtr<CefBrowser> browser) OVERRIDE;
-	virtual CefRefPtr<CefLoadHandler> GetLoadHandler() OVERRIDE;
-	virtual bool OnBeforeNavigation(
-		CefRefPtr<CefBrowser> browser,
-		CefRefPtr<CefFrame> frame,
-		CefRefPtr<CefRequest> request,
-		NavigationType navigation_type,
-		bool is_redirect) OVERRIDE;
-	virtual void OnContextCreated(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> context) OVERRIDE;
-	virtual void OnContextReleased(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> context) OVERRIDE;
-	virtual void OnUncaughtException(
-		CefRefPtr<CefBrowser> browser,
-		CefRefPtr<CefFrame> frame,
-		CefRefPtr<CefV8Context> context,
-		CefRefPtr<CefV8Exception> exception,
-		CefRefPtr<CefV8StackTrace> stackTrace) OVERRIDE;
-	virtual void OnFocusedNodeChanged(
-		CefRefPtr<CefBrowser> browser,
-		CefRefPtr<CefFrame> frame,
-		CefRefPtr<CefDOMNode> node) OVERRIDE;
-	virtual bool OnProcessMessageReceived(
-		CefRefPtr<CefBrowser> browser,
-		CefProcessId source_process,
-		CefRefPtr<CefProcessMessage> message) OVERRIDE;
-private:
-	std::shared_ptr<CefJSBridge>	render_js_bridge_;
-	std::vector<CefString>			cookieable_schemes_;
-	// Schemes that will be registered with the global cookie manager. Used in
-	bool							last_node_is_editable_;
-	// both the browser and renderer process.
-	IMPLEMENT_REFCOUNTING(ClientApp);
-};
+        // CefRenderProcessHandler methods.
+        virtual void OnRenderThreadCreated(CefRefPtr<CefListValue> extra_info) OVERRIDE;
+        virtual void OnWebKitInitialized() OVERRIDE;
+        virtual void OnBrowserCreated(CefRefPtr<CefBrowser> browser) OVERRIDE;
+        virtual void OnBrowserDestroyed(CefRefPtr<CefBrowser> browser) OVERRIDE;
+        virtual CefRefPtr<CefLoadHandler> GetLoadHandler() OVERRIDE;
+        virtual bool OnBeforeNavigation(
+            CefRefPtr<CefBrowser> browser,
+            CefRefPtr<CefFrame> frame,
+            CefRefPtr<CefRequest> request,
+            NavigationType navigation_type,
+            bool is_redirect) OVERRIDE;
+        virtual void OnContextCreated(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> context) OVERRIDE;
+        virtual void OnContextReleased(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> context) OVERRIDE;
+        virtual void OnUncaughtException(
+            CefRefPtr<CefBrowser> browser,
+            CefRefPtr<CefFrame> frame,
+            CefRefPtr<CefV8Context> context,
+            CefRefPtr<CefV8Exception> exception,
+            CefRefPtr<CefV8StackTrace> stackTrace) OVERRIDE;
+        virtual void OnFocusedNodeChanged(
+            CefRefPtr<CefBrowser> browser,
+            CefRefPtr<CefFrame> frame,
+            CefRefPtr<CefDOMNode> node) OVERRIDE;
+        virtual bool OnProcessMessageReceived(
+            CefRefPtr<CefBrowser> browser,
+            CefProcessId source_process,
+            CefRefPtr<CefProcessMessage> message) OVERRIDE;
+      private:
+        std::shared_ptr<CefJSBridge>	render_js_bridge_;
+        std::vector<CefString>			cookieable_schemes_;
+        // Schemes that will be registered with the global cookie manager. Used in
+        bool							last_node_is_editable_;
+        // both the browser and renderer process.
+        IMPLEMENT_REFCOUNTING(ClientApp);
+    };
 }
--- ui_components/cef_control/control/cef_control.cpp
+++ ui_components/cef_control/control/cef_control.cpp
@@ -4,51 +4,47 @@
 
 namespace nim_comp {
 
-void CefControl::OnPaint(CefRefPtr<CefBrowser> browser, CefRenderHandler::PaintElementType type, const CefRenderHandler::RectList& dirtyRects, const std::string* buffer, int width, int height)
-{
-	if (NULL == buffer)
-		return;
+    void CefControl::OnPaint(CefRefPtr<CefBrowser> browser, CefRenderHandler::PaintElementType type, const CefRenderHandler::RectList& dirtyRects, const std::string* buffer, int width, int height) {
+        if (NULL == buffer)
+            return;
 
-	if (type == PET_VIEW)
-	{
-		if (dc_cef_.GetWidth() != width || dc_cef_.GetHeight() != height)
-			dc_cef_.Init(m_pWindow->GetPaintDC(), width, height);
+        if (type == PET_VIEW) {
+            if (dc_cef_.GetWidth() != width || dc_cef_.GetHeight() != height)
+                dc_cef_.Init(m_pWindow->GetPaintDC(), width, height);
 
-		LPBYTE pDst = (LPBYTE)dc_cef_.GetBits();
-		if (pDst)
-			memcpy(pDst, (char*)buffer->c_str(), height * width * 4);
-	}
-	else if (type == PET_POPUP && dc_cef_.IsValid() && rect_popup_.width > 0 && rect_popup_.height > 0)
-	{
-		// 单独保存popup窗口的位图
-		if (dc_cef_popup_.GetWidth() != width || dc_cef_popup_.GetHeight() != height)
-			dc_cef_popup_.Init(m_pWindow->GetPaintDC(), width, height);
+            LPBYTE pDst = (LPBYTE)dc_cef_.GetBits();
 
-		LPBYTE pDst = (LPBYTE)dc_cef_popup_.GetBits();
-		if (pDst)
-			memcpy(pDst, (char*)buffer->c_str(), width * height * 4);
-	}
+            if (pDst)
+                memcpy(pDst, (char*)buffer->c_str(), height * width * 4);
 
-	this->Invalidate();
-}
+        } else if (type == PET_POPUP && dc_cef_.IsValid() && rect_popup_.width > 0 && rect_popup_.height > 0) {
+            // 单独保存popup窗口的位图
+            if (dc_cef_popup_.GetWidth() != width || dc_cef_popup_.GetHeight() != height)
+                dc_cef_popup_.Init(m_pWindow->GetPaintDC(), width, height);
 
-void CefControl::OnPopupShow(CefRefPtr<CefBrowser> browser, bool show)
-{
-	if (!show)
-	{
-		// 当popup窗口隐藏时，刷新popup区域
-		CefRect rect_dirty = rect_popup_;
-		rect_popup_.Set(0, 0, 0, 0);
-		browser->GetHost()->Invalidate(PET_VIEW);
-	}
-}
+            LPBYTE pDst = (LPBYTE)dc_cef_popup_.GetBits();
 
-void CefControl::OnPopupSize(CefRefPtr<CefBrowser> browser, const CefRect& rect)
-{
-	if (rect.width <= 0 || rect.height <= 0)
-		return;
+            if (pDst)
+                memcpy(pDst, (char*)buffer->c_str(), width * height * 4);
+        }
 
-	rect_popup_ = rect;
-}
+        this->Invalidate();
+    }
+
+    void CefControl::OnPopupShow(CefRefPtr<CefBrowser> browser, bool show) {
+        if (!show) {
+            // 当popup窗口隐藏时，刷新popup区域
+            CefRect rect_dirty = rect_popup_;
+            rect_popup_.Set(0, 0, 0, 0);
+            browser->GetHost()->Invalidate(PET_VIEW);
+        }
+    }
+
+    void CefControl::OnPopupSize(CefRefPtr<CefBrowser> browser, const CefRect& rect) {
+        if (rect.width <= 0 || rect.height <= 0)
+            return;
+
+        rect_popup_ = rect;
+    }
 
 }
