4f99dd59be2762eb4a89d6ec459c5a17828df10a
许迎春 <yingchun.xu@china.zhaogang.com>
2020-09-22 20:21:31 +0800

    format

--- ui_components/cef_control/app/js_handler.h
+++ ui_components/cef_control/app/js_handler.h
@@ -9,20 +9,20 @@
 #include "include/cef_app.h"
 #include "cef_control/app/cef_js_bridge.h"
 
-namespace nim_comp
-{
-	
-class CefJSHandler : public CefV8Handler
-{
-public:
-	CefJSHandler() {}
-	virtual bool Execute(const CefString& name,	CefRefPtr<CefV8Value> object, const CefV8ValueList& arguments, CefRefPtr<CefV8Value>& retval, CefString& exception) OVERRIDE;
-	void AttachJSBridge(std::shared_ptr<CefJSBridge> js_bridge) { js_bridge_ = js_bridge; }
+namespace nim_comp {
 
-	IMPLEMENT_REFCOUNTING(CefJSHandler);
+    class CefJSHandler : public CefV8Handler {
+      public:
+        CefJSHandler() {}
+        virtual bool Execute(const CefString& name,	CefRefPtr<CefV8Value> object, const CefV8ValueList& arguments, CefRefPtr<CefV8Value>& retval, CefString& exception) OVERRIDE;
+        void AttachJSBridge(std::shared_ptr<CefJSBridge> js_bridge) {
+            js_bridge_ = js_bridge;
+        }
 
-private:
-	std::shared_ptr<CefJSBridge> js_bridge_;
-};
+        IMPLEMENT_REFCOUNTING(CefJSHandler);
+
+      private:
+        std::shared_ptr<CefJSBridge> js_bridge_;
+    };
 }
 
